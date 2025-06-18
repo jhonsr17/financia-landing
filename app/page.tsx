b9c6bd86c5204bf5532ceeb2693ab16984b61d9c
Jhons <jhonsrr17@gmail.com>
2025-06-17 21:25:40 -0500

    Fix: Restore app/page.tsx with basic content

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,17 @@
- 
+import { FloatingChat } from '@/components/FloatingChat'
+import { WaitlistForm } from '@/components/WaitlistForm'
+import ChatCarousel from '@/components/ChatCarousel'
+import StatsSection from '@/components/StatsSection'
+import FeaturesGrid from '@/components/FeaturesGrid'
+
+export default function Home() {
+  return (
+    <main className='min-h-screen bg-[#0D1D35]'>
+      <FloatingChat />
+      <ChatCarousel />
+      <StatsSection />
+      <FeaturesGrid />
+      <WaitlistForm />
+    </main>
+  )
+}
